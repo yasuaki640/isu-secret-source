2fe0870ac966d78fb14fbe65d81ce52287c0574f
yasuaki640 <yasuaki640@gmail.com>
2024-12-07 23:55:17 +0900

    その場しのぎ2

--- scripts/remote/reload-nginx-conf.sh
+++ scripts/remote/reload-nginx-conf.sh
@@ -3,10 +3,3 @@
 sudo nginx -t # 検証
 
 sudo systemctl reload nginx # 設定変更反映
-
-# TODO: 被っているので後でなんとかする
-# ログファイルローテート
-sudo mv /var/log/nginx/access.log /var/log/nginx/access.log.`date +%Y%m%d-%H%M%S`
-
-# nginxにログファイルを開き直すシグナルを送信
-sudo nginx -s reopen
