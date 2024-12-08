3b36239007bebd574365f7ec2f1b001d877ac826
yasuaki640 <yasuaki640@gmail.com>
2024-12-08 11:47:41 +0900

    sdf

--- scripts/remote/dump-mysql-slow-log.sh
+++ scripts/remote/dump-mysql-slow-log.sh
@@ -10,4 +10,4 @@ timestamp=$(date +%Y%m%d-%H%M%S)-$$
 mkdir -p "$HOME/log/mysql-slow"
 
 # pt-query-digest を実行し、ログを出力
-sudo pt-query-digest /var/log/mysql/mysql-slow.log | sudo tee "$HOME/log/mysql-slow/pt-query.log.$timestamp" > /dev/null
+sudo pt-query-digest /var/log/mysql/mysql-slow.log | sudo tee "$HOME/log/mysql-slow/pt-query.log.$timestamp"
